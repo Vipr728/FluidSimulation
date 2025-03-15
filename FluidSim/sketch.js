85d09066d690c355a9d40ba8ecce918e72986d9d
floatedbloom <georgelapin23@gmail.com>
2025-03-15 14:11:48 -0700

    grid

--- FluidSim/sketch.js
+++ FluidSim/sketch.js
@@ -6,7 +6,7 @@ function setup()
 {
     createCanvas(800, 600);
     background(0);
-    grid = Array.from({ length: displayWidth/size }, () => Array(displayHeight/size).fill(0))
+    grid = Array.from({ length: width/size }, () => Array(height/size).fill(0))
     u = grid.length;
     v = grid[0].length;
     initCells();
@@ -26,7 +26,8 @@ function mouseDragged() {
 
 
 function drawCells(grid){
-    for(row of grid){
+    gridInstance = new Grid(grid,20,20);
+    for(row of gridInstance.cellList){
         for(cell of row){
             stroke( 255, 255, 255);
             fill(0,0,255, cell.value);
@@ -58,21 +59,22 @@ class Cell{
         this.value = value;
     }
 }
-/*
+
 class Grid {
-    constructor() {
-      this.ylen = 12;
-      this.xlen = 12;
-      this.cells = ylen * xlen;
-      this.u = new Float32Array(this.cells); // horizontal velo at each location
-      this.v = new Float32Array(this.cells); // vertical velo at each location
-      this.newU = new Float32Array(this.cells); //  temp storage for horizaontal velo
-      this.newV = new Float32Array(this.cells); // temp storage for vertical velo
-      this.p = new Float32Array(this.cells); // pressure at each location
-      this.s = new Float32Array(this.cells); // solid or not at each location (0 is solid)
-      this.m = new Float32Array(this.cells); // mass at each location
-      this.newM = new Float32Array(this.cells); // temp storage for masses
-      this.m.fill(1.0);
+    constructor(arr,xlen,ylen) {
+        this.cellList = arr;
+        this.ylen = ylen;
+        this.xlen = xlen;
+        this.numCells = ylen * xlen;
+        this.u = new Float32Array(this.numCells); // horizontal velo at each location
+        this.v = new Float32Array(this.numCells); // vertical velo at each location
+        this.newU = new Float32Array(this.numCells); //  temp storage for horizaontal velo
+        this.newV = new Float32Array(this.numCells); // temp storage for vertical velo
+        this.p = new Float32Array(this.numCells); // pressure at each location
+        this.s = new Float32Array(this.numCells); // solid or not at each location (0 is solid)
+        this.m = new Float32Array(this.numCells); // mass at each location
+        this.newM = new Float32Array(this.numCells); // temp storage for masses
+        this.m.fill(1.0);
     }
     
     integrate(dt, grav) {
@@ -88,4 +90,4 @@ class Grid {
         }
     }
     
-  }*/
+  }
