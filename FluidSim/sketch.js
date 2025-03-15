0012ea0fee3a867acc34abfdfa44f3fdd0932d25
floatedbloom <georgelapin23@gmail.com>
2025-03-15 13:16:50 -0700

    integration

--- FluidSim/sketch.js
+++ FluidSim/sketch.js
@@ -59,3 +59,33 @@ class Cell{
     }
 }
 
+class Grid {
+    constructor() {
+      this.ylen = 12;
+      this.xlen = 12;
+      this.cells = ylen * xlen;
+      this.u = new Float32Array(this.numCells); // horizontal velo at each location
+      this.v = new Float32Array(this.numCells); // vertical velo at each location
+      this.newU = new Float32Array(this.numCells); //  temp storage for horizaontal velo
+      this.newV = new Float32Array(this.numCells); // temp storage for vertical velo
+      this.p = new Float32Array(this.numCells); // pressure at each location
+      this.s = new Float32Array(this.numCells); // solid or not at each location (0 is solid)
+      this.m = new Float32Array(this.numCells); // mass at each location
+      this.newM = new Float32Array(this.numCells); // temp storage for masses
+      this.m.fill(1.0);
+    }
+    
+    integrate(dt, grav) {
+        //assumes 1 cell buffer on outside of grid
+        for (let i = 1; i <= this.xlen-1; i++) {
+            for (let j = 1; j < this.ylen-1; j++) {
+                if (this.s[i * ylen + j] != 0.0 && this.s[i * ylen + j - 1] != 0.0) {
+                    //^^ checks if this node is on the ground (s is 1d list) (v is 1d list)
+                    let v_old = this.v[i * ylen + j];
+                    this.v[i * ylen + j] = v_old + grav*dt; // basic euler estimation
+                }
+            }
+        }
+    }
+    
+  }
