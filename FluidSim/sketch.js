3a772ab2ed4f83f30b67127e18e007f8926b04a4
Abhi <vipr728@gmail.com>
2025-03-15 20:35:27 -0700

    refactor: adjust grid size and add SVG parsing functionality

--- FluidSim/sketch.js
+++ FluidSim/sketch.js
@@ -1,16 +1,24 @@
 let grid = [];
-let size = 50;
+let size = 20;
 let u = [];
 let v = [];
+let add = false;
+let svgWidth = 20;
+let svgHeight = 20;
+let svg = [];
 function setup() 
 {
     createCanvas(800, 600);
     background(0);
-    grid = Array.from({ length: displayWidth/size }, () => Array(displayHeight/size).fill(0))
+    grid = Array.from({ length: width/size }, () => Array(height/size).fill(0))
     u = grid.length;
     v = grid[0].length;
+    svg = loadStrings("arrow.txt");
     initCells();
     drawCells(grid);
+    let toggle = createButton("Add Circle")
+    toggle.mousePressed(function() {
+        add = !add;});
 }
 
 function draw()
@@ -21,9 +29,15 @@ function draw()
 function mouseDragged() {
     let col = Math.floor(mouseX/ size);
     let row = Math.floor(mouseY/ size);
+    if (col >= grid.length || row >= grid[0].length) return [-1,-1]; // Check bounds
     grid[col][row].value = 255; // Set the cell value to white
 }
 
+function mousePressed() {
+    if (add) {
+        parseSVG(svg[0], mouseX, mouseY);
+    }
+}
 
 function drawCells(grid){
     for(row of grid){
@@ -49,7 +63,30 @@ function UpdateCells(u, v, grid){
     for(let x = 0; x< u; x++){ 
     }
 }
-
+function parseSVG(svgString, centerX, centerY) {
+    let parser = new DOMParser();
+    const doc = parser.parseFromString(svgString, "image/svg+xml");
+    const svgElement = doc.querySelector('svg');
+    if (!svgElement) {
+        console.error("Error parsing SVG: No SVG element found");
+        return;
+    }
+    let paths = svgElement.querySelectorAll('path');
+    let points = [];
+    paths.forEach(path => {
+        for (let i = 0; i < 1000; i++) {
+            const point = path.getPointAtLength(i * 0.001 * path.getTotalLength());
+            const adjustedX = (centerX-svgWidth) + point.x * size/1;
+            const adjustedY = (centerY-svgWidth) + point.y * size/1;
+            stroke(255, 0, 0);
+            fill(255, 0, 0);
+            points.push({ x: adjustedX, y: adjustedY });
+            circle(adjustedX, adjustedY, 5);
+            console.log(adjustedX, adjustedY);
+        }
+    });
+    return points;
+}
 class Cell{
     constructor(x, y, size, value){
         this.x = x;
